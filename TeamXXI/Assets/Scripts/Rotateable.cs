0bdd3f734dc5f619258dadce3c5799991ed28d90
Felix Stieglbauer <felix@stieglbauer.de>
2020-12-11 21:38:14 +0100

    ini

---
+++ TeamXXI/Assets/Scripts/Rotateable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotateable : MonoBehaviour
+{
+    [SerializeField]
+    private float angle;
+    [SerializeField]
+    private Vector3 axis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.rotation = Quaternion.AngleAxis(angle, axis);
+    }
+
+    public float getAngle()
+    {
+        return angle;
+    }
+
+    public void setAngle(float angle)
+    {
+        this.angle = angle;
+    }
+}
