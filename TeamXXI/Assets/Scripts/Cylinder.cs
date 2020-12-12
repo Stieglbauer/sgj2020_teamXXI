0100725a0f7ce1f0c3209959bff7973621113194
Felix Stieglbauer <felix@stieglbauer.de>
2020-12-12 01:43:04 +0100

    added cylinder and exhaust

---
+++ TeamXXI/Assets/Scripts/Cylinder.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cylinder : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject cylinder_center, cylinder_direction;
+
+    [SerializeField]
+    private float max, speed, progress;
+
+    private Vector3 start, end, bar_offset;
+    private int direction = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bar_offset = cylinder_center.transform.position - gameObject.transform.position;
+        start = gameObject.transform.position;
+        end = gameObject.transform.position + Vector3.down * max;
+        gameObject.transform.Translate(progress * (end - start), Space.World);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(gameObject.transform.position, end) >= max && Vector3.Distance(gameObject.transform.position, end) >= max)
+        {
+            direction = Vector3.Distance(gameObject.transform.position, end) > Vector3.Distance(gameObject.transform.position, start) ? 1 : -1;
+        } else if (Vector3.Distance(gameObject.transform.position, end) >= max)
+        {
+            direction = 1;
+        } else if(Vector3.Distance(gameObject.transform.position, start) >= max)
+        {
+            direction = -1;
+        }
+        gameObject.transform.Translate(direction * Time.deltaTime * speed * (end - start).normalized, Space.World);
+        cylinder_center.transform.position = gameObject.transform.position + bar_offset;
+        cylinder_center.transform.rotation = Quaternion.LookRotation(cylinder_center.transform.position - cylinder_direction.transform.position, Vector3.up);
+
+    }
+}
---
+++ TeamXXI/Assets/Scripts/Shakeable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shakeable : MonoBehaviour
+{
+    [SerializeField]
+    private float rotationDelta;
+    [SerializeField]
+    private float positionDelta;
+    private Quaternion initialRotation;
+    private Vector3 initialPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.initialRotation = Quaternion.Inverse(gameObject.transform.parent.rotation) * gameObject.transform.rotation;
+        this.initialPosition = gameObject.transform.position - gameObject.transform.parent.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.SetPositionAndRotation(gameObject.transform.parent.position + initialPosition + new Vector3(Random.Range(-positionDelta, positionDelta), Random.Range(-positionDelta, positionDelta), Random.Range(-positionDelta, positionDelta)), gameObject.transform.parent.rotation * initialRotation * new Quaternion(Random.Range(-rotationDelta, rotationDelta), Random.Range(-rotationDelta, rotationDelta), Random.Range(-rotationDelta, rotationDelta), 1).normalized);
+    }
+}
