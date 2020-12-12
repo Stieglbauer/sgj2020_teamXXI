83685d7d4d917326eee241e28c01726d516fa220
Felix Stieglbauer <felix@stieglbauer.de>
2020-12-12 21:46:19 +0100

    enemy movement

---
+++ TeamXXI/Assets/Scripts/EnemyPathFollowing.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathFollowing : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject path;
+    private GameObject currentTarget;
+
+    private Pathfinding pf;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pf = path.GetComponent<Pathfinding>();
+        currentTarget = pf.getNextCheckpoint(null);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0.01f * (currentTarget.transform.position - transform.position));
+        if(Vector3.Distance(transform.position, currentTarget.transform.position) < 1)
+        {
+            Debug.Log("next!");
+            currentTarget = pf.getNextCheckpoint(currentTarget);
+        }
+        if(currentTarget == null)
+        {
+            Debug.Log("Boom!");
+            GameObject.Destroy(this);
+        }
+    }
+}
---
+++ TeamXXI/Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] checkpoints;
+
+    public GameObject getNextCheckpoint(GameObject current)
+    {
+        if(current == null)
+        {
+            return checkpoints[0];
+        }
+        for(int i=0; i<checkpoints.Length-1; i++)
+        {
+            if(current.Equals(checkpoints[i]))
+            {
+                return checkpoints[i + 1];
+            }
+        }
+        return null;
+    }
+}
