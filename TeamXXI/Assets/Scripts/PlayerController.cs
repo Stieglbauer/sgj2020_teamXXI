5ee60e726e7358344820755410bdefd0cf8843e7
JonesPawa <dev@aoyoa.net>
2020-12-12 13:23:19 +0100

    Added Player scripts

---
+++ TeamXXI/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController characterController;
+    public float MovementSpeed = 1;
+    public float Gravity = 9.8f;
+    private float velocity = 0;
+    [SerializeField]
+    private GameObject cam;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // player movement - forward, backward, left, right
+        float horizontal = Input.GetAxis("Horizontal") * MovementSpeed;
+        float vertical = Input.GetAxis("Vertical") * MovementSpeed;
+        characterController.Move((cam.transform.right * horizontal + cam.transform.forward * vertical) * Time.deltaTime);
+
+        // Gravity
+        if (characterController.isGrounded)
+        {
+            velocity = 0;
+        }
+        else
+        {
+            velocity -= Gravity * Time.deltaTime;
+            characterController.Move(new Vector3(0, velocity, 0));
+        }
+    }
+}
