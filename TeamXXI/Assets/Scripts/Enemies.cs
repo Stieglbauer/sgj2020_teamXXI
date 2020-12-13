0121c0cc75515a1eaaf79fd241110a65a4fe041f
Felix Stieglbauer <felix@stieglbauer.de>
2020-12-13 09:53:09 +0100

    gegner spawn script

--- TeamXXI/Assets/Scripts/Enemies.cs
+++ TeamXXI/Assets/Scripts/Enemies.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class Enemies : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject path;
+
+    private double counter = 0;
+    private int schedule_progress = 0;
+    [SerializeField]
+    private GameObject[] enemyTypes;
+
+    [SerializeField]
+    private float[] schedule;
+
+    [SerializeField]
+    private int[] types;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +27,30 @@ public class Enemies : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        counter += Time.deltaTime;
+        if (schedule_progress >= schedule.Length) {
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            int leviathans = 0;
+            foreach(GameObject enemy in enemies)
+            {
+                if(enemy.GetComponent<EnemyPathFollowing>() != null)
+                {
+                    leviathans++;
+                    break;
+                }
+            }
+            if(leviathans == 0)
+            {
+                Debug.Log("Win");
+            }
+        } else {
+            if (counter > schedule[schedule_progress])
+            {
+                GameObject tank = Instantiate(enemyTypes[types[schedule_progress]], this.transform.position, enemyTypes[types[schedule_progress]].transform.rotation);
+                tank.GetComponent<EnemyPathFollowing>().setPath(path);
+                schedule_progress++;
+                counter = 0;
+            }
+        }
     }
 }
---
+++ TeamXXI/Assets/Scripts/ladder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ladder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+        }
+    }
+}
