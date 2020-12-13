c220d62cb931a1deafeb7120300fe5ee3ca65ef6
Karl Kraus <karl.kraus@tum.de>
2020-12-13 11:17:16 +0100

    lightning sound

---
+++ TeamXXI/Assets/Scripts/LightningSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningSound : MonoBehaviour
+{
+    public AudioSource source;
+    public float startDelay;
+
+    void Start()
+    {
+        source.PlayDelayed(startDelay);
+    }
+
+}
